c8a0906de58174b5f5bf2a34cbee23246bebd9be
Yonava <yonavossandreae@gmail.com>
2022-10-18 02:46:17 -0400

    vue-project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="numOfCounters++">Add a Counter</button>
+    <button @click="numOfCounters--">Remove a Counter</button>
+    <div v-for="i in numOfCounters" :key="i">
+      <Counter />
+    </div>
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter.vue'
+export default {
+  components: { Counter },
+  data() {
+    return {
+      numOfCounters: 3
+    }
+  }
+}
+</script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <p v-if="count >= 10">top secret message!</p>
+    <p v-else-if="count >= 5">kinda secret message...</p>
+    <p v-else>there are no secret messages</p>
+    <input type="number" v-model="count">
+    <br>
+    <button @click="count++">Increment Count</button>
+    <br>
+    <button @click="count--">Decrement Count</button>
+    <br>
+    <button @click="double">Double Count</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    double() {
+      this.count *= 2
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
